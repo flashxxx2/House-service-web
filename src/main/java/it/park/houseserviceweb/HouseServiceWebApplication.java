29f68e4b6dfb778855af2579541ed190b673f41d
Aleksander <rit>
2018-12-18 16:14:48 +0300

    Начало

---
+++ src/main/java/it/park/houseserviceweb/HouseServiceWebApplication.java
@@ -0,0 +1,14 @@
+package it.park.houseserviceweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseServiceWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseServiceWebApplication.class, args);
+    }
+
+}
+
